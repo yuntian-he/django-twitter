362d5222fd5a97370ec0ebda2a39eb20a9e1d750
matthew_he <hytian994@mail.com>
2022-06-20 22:40:50 -0400

    Added comments creation

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
+from tweets.models import Tweet
 
 # Register your models here.
+@admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     list_display = ('created_at',
