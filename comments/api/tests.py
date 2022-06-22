1b1c5a566c3dc4d417624996e462d2df25fc4b39
matthew_he <hytian994@mail.com>
2022-06-21 21:13:36 -0400

    Added comment creating API

--- comments/api/tests.py
+++ comments/api/tests.py
@@ -1,48 +1,53 @@
 from testing.testcases import TestCase
 from rest_framework.test import APIClient
 
+
 COMMENT_URL = '/api/comments/'
 
 
 class CommentApiTests(TestCase):
 
     def setUp(self):
-        self.user1 = self.create_user('linghu')
-        self.user1_client = APIClient()
-        self.user1_client.force_authenticate(self.user1)
+        self.linghu = self.create_user('linghu')
+        self.linghu_client = APIClient()
+        self.linghu_client.force_authenticate(self.linghu)
+        self.dongxie = self.create_user('dongxie')
+        self.dongxie_client = APIClient()
+        self.dongxie_client.force_authenticate(self.dongxie)
 
-        self.user2 = self.create_user('dongxie')
-        self.user2_client = APIClient()
-        self.user2_client.force_authenticate(self.user2)
-
-        self.tweet = self.create_tweet(self.user1)
+        self.tweet = self.create_tweet(self.linghu)
 
     def test_create(self):
+        # 匿名不可以创建
         response = self.anonymous_client.post(COMMENT_URL)
         self.assertEqual(response.status_code, 403)
 
-        response = self.user1_client.post(COMMENT_URL)
+        # 啥参数都没带不行
+        response = self.linghu_client.post(COMMENT_URL)
         self.assertEqual(response.status_code, 400)
 
-        response = self.user1_client.post(COMMENT_URL, {'tweet_id': self.tweet.id})
+        # 只带 tweet_id 不行
+        response = self.linghu_client.post(COMMENT_URL, {'tweet_id': self.tweet.id})
         self.assertEqual(response.status_code, 400)
 
-        response = self.user1_client.post(COMMENT_URL, {'content': '1'})
+        # 只带 content 不行
+        response = self.linghu_client.post(COMMENT_URL, {'content': '1'})
         self.assertEqual(response.status_code, 400)
 
-        response = self.user1_client.post(COMMENT_URL, {
+        # content 太长不行
+        response = self.linghu_client.post(COMMENT_URL, {
             'tweet_id': self.tweet.id,
             'content': '1' * 141,
         })
         self.assertEqual(response.status_code, 400)
         self.assertEqual('content' in response.data['errors'], True)
 
-        response = self.user1_client.post(COMMENT_URL, {
+        # tweet_id 和 content 都带才行
+        response = self.linghu_client.post(COMMENT_URL, {
             'tweet_id': self.tweet.id,
             'content': '1',
         })
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data['user']['id'], self.user1.id)
+        self.assertEqual(response.data['user']['id'], self.linghu.id)
         self.assertEqual(response.data['tweet_id'], self.tweet.id)
         self.assertEqual(response.data['content'], '1')
-
